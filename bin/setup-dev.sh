ba8245a4ef69a3d155f92a9670fc471abfc0ef51
Frederic Morin <frederic.morin.8@gmail.com>
2026-03-04 04:24:46 -0500

    use venv instead of .venv

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -5,9 +5,9 @@ log() { echo "$(tput setaf 2)[ok]$(tput sgr0) $@"; }
 SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd -P )"
 
 cd "$SCRIPT_ROOT"
-[ ! -e "$SCRIPT_ROOT/.venv" ] && log "Setup venv" && python3 -m venv --prompt venv .venv
-[ ! -e "$SCRIPT_ROOT/.venv/bin/uv" ] && log "Install uv" && "$SCRIPT_ROOT/.venv/bin/pip" install uv
-source "$SCRIPT_ROOT/.venv/bin/activate"
+[ ! -e "$SCRIPT_ROOT/venv" ] && log "Setup venv" && python3 -m venv --prompt venv venv
+[ ! -e "$SCRIPT_ROOT/venv/bin/uv" ] && log "Install uv" && "$SCRIPT_ROOT/venv/bin/pip" install uv
+source "$SCRIPT_ROOT/venv/bin/activate"
 
 log "Install project as editable"
 uv pip install -U --editable .[dev]
